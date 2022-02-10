42616eec24af0b68fd2895e97cda5fec68ef196d
Daulet Ashikbayev <dxulet@gmail.com>
2022-02-10 18:14:53 +0500

    Initial Commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Daulet Ashikbayev on 10.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
